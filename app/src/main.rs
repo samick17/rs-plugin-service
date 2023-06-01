4495d82e7385c4159ffbb0d8b86aad61017236c5
samick.hsu <samick.hsu@gmail.com>
2023-06-01 21:19:39 +0800

    1. Added spawn process code

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,11 +1,22 @@
 // mod main_mp;
 // mod main_runner;
 // mod main_watcher;
-mod main_spider;
+// mod main_spider;
+use std::process::{Command};
+use std::thread;
+use std::time::{Duration};
 
 fn main() {
     // main_mp::main();
     // main_runner::main();
     // main_watcher::main();
-    main_spider::main();
+    // main_spider::main();
+    let mut child = Command::new("ping")
+    .arg("localhost")
+    .spawn()
+    .expect("Failed to spawn child process");
+
+    thread::sleep(Duration::from_secs(3));
+    child.kill();
+    child.wait().unwrap();
 }
