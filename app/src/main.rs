128d61465c6a4c8746a7d1cd8610a5073ea7a56d
samick.hsu <samick.hsu@gmail.com>
2023-06-15 20:53:55 +0800

    1. Reopen message queue main program 2. Added main_consumer

--- app/src/main.rs
+++ app/src/main.rs
@@ -3,11 +3,12 @@
 // mod main_watcher;
 // mod main_spider;
 // mod main_spawn;
-// mod main_broker;
-// mod main_producer;
-mod main_wasm;
+mod main_broker;
+mod main_producer;
+mod main_consumer;
+// mod main_wasm;
 
-// use std::env;
+use std::env;
 
 
 fn main() {
@@ -16,22 +17,23 @@ fn main() {
     // main_watcher::main();
     // main_spider::main();
     // main_spawn::main();
-    main_wasm::main();
-    // let role = env::var("ROLE").unwrap_or(String::from("default"));
-    // match role.as_str() {
-    //     "broker" => {
-    //         println!("[Broker]");
-    //         main_broker::main();
-    //     },
-    //     "producer" => {
-    //         println!("[Producer]");
-    //         main_producer::main();
-    //     },
-    //     "consumer" => {
-    //         println!("[Consumer]");
-    //     },
-    //     _ => {
-    //         println!("No role specified.");
-    //     },
-    // }
+    // main_wasm::main();
+    let role = env::var("ROLE").unwrap_or(String::from("default"));
+    match role.as_str() {
+        "broker" => {
+            println!("[Broker]");
+            main_broker::main();
+        },
+        "producer" => {
+            println!("[Producer]");
+            main_producer::main();
+        },
+        "consumer" => {
+            println!("[Consumer]");
+            main_consumer::main();
+        },
+        _ => {
+            println!("No role specified.");
+        },
+    }
 }
